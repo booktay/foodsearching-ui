bed1af71226ff293a943c6db2d8eca6be90ce230
Siwanont Sittinam <siwanont1@gmail.com>
2020-06-07 20:57:02 +0700

    Add Seachkeyword page

--- src/pages/SearchKeyword.js
+++ src/pages/SearchKeyword.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Container } from "@material-ui/core";
@@ -9,8 +9,12 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Pagination from '@material-ui/lab/Pagination';
+
+const axios = require('axios');
 
 const styles = theme => ({
     root: {
@@ -20,12 +24,15 @@ const styles = theme => ({
         alignItems: 'center',
         minHeight: '100vh'
     },
+    grow: {
+        flexGrow: 1,
+    },
     searchroot: {
         padding: '2px 4px',
         display: 'inline-flex',
         alignItems: 'center',
-        width: '70vw',
-        height: '7vh',
+        width: '73vw',
+        minHeight: '7vh',
     },
     input: {
         marginLeft: theme.spacing(2),
@@ -38,17 +45,35 @@ const styles = theme => ({
         height: 28,
         margin: 4,
     },
-    cardroot: {
+    cardTotalroot: {
         display: 'inline-block',
-        width: '70vw',
-        minHeight: '40vh',
+        width: '20vw',
+        minHeight: '10vh',
         textAlign: 'left',
         color: '#1b6ca8',
+    },
+    cardTotalcontent: {
+        paddingBottom: '24px',
+    },
+    cardroot: {
+        display: 'inline-block',
+        width: '73vw',
+        textAlign: 'left',
+        color: '#1b6ca8',
+    },
+    cardcontent: {
+        paddingTop: 0,
     },
     reviewtext: {
         textAlign: 'left',
         padding: '10px 20px 10px 20px',
         color: '#000000',
+    },
+    modifieddate: {
+        textAlign: 'right',
+    },
+    paginationroot: {
+        display: 'inline-flex',
     }
 });
 
@@ -57,53 +82,221 @@ class SearchKeyword extends Component {
         super();
 
         this.state = {
-            foodKeyword: "ข้าวมันไก่",
-            reviewText: "Example Text",
+            searchKeyword: "",
+            foodKeyword: null,
+            resultDatas: null,
+            errorMessage: null,
+            maxScore: null,
+            page: 1,
         }
 
+        this.getdata = this.getdata.bind(this)
+        this.handleClickSearch = this.handleClickSearch.bind(this);
+
+        this.searchBox = this.searchBox.bind(this);
+        this.resultCard = this.resultCard.bind(this);
+        this.totalCard = this.totalCard.bind(this);
+        this.handleEmpty = this.handleEmpty.bind(this);
+        this.handlePagination = this.handlePagination.bind(this);
+        this.handleHasresult = this.handleHasresult.bind(this);
+        this.handleNoresult = this.handleNoresult.bind(this);
     }
+
+    handleChangePage = (event, value) => {
+        this.setState(state => {
+            state.page = value;
+            return state
+        });
+    }
+
+    handleClickSearch() {
+        const { searchKeyword } = this.state
+
+        if (searchKeyword !== "") {
+            this.getdata(searchKeyword)
+        } else {
+            this.setState(state => {
+                state.errorMessage = "Input new keyword";
+                return state
+            });
+        }
+    }
+
+    async getdata(foodKeyword) {
+        axios.defaults.baseURL = 'http://localhost:5555'
+        const response = await axios.get('/reviews', {
+            "params" : {
+                "query": foodKeyword,
+            }
+        })
+        console.log(response)
+        if (response.status === 200) {
+            
+            if (response.data["hits"]) {
+                this.setState(state => {
+                    state.foodKeyword = foodKeyword;
+                    state.maxScore = response.data['max_score'];
+                    state.resultDatas = response.data['hits'];
+                    state.errorMessage = null;
+                    return state
+                });
+            } else {
+                this.setState(state => {
+                    state.errorMessage = response.data["message"];
+                    state.resultDatas = null;
+                    return state
+                });
+            }
+        }
+    }
+
+    searchBox() {
+        const { classes } = this.props;
+        const { searchKeyword } = this.state;
+
+        return (
+            <Grid item xs={12}>
+                <Paper component="form" className={classes.searchroot}>
+                    <InputBase
+                        className={classes.input}
+                        placeholder="Search Food Reviews Ex. ข้าวมันไก่ เนื้อน่อง ตับเครื่องใน"
+                        value={searchKeyword}
+                        onChange={e => this.setState({ searchKeyword: e.target.value })}
+                    />
+                    <Divider className={classes.divider} orientation="vertical" />
+                    <IconButton color="primary" className={classes.iconButton} aria-label="directions" onClick={this.handleClickSearch}>
+                        <SearchIcon />
+                    </IconButton>
+                </Paper>
+            </Grid>
+        );
+    }
+
+    totalCard() {
+        const { classes } = this.props;
+        const { foodKeyword, maxScore } = this.state;
+
+        return (
+            <Fragment>
+                <Grid item xs={6} className={classes.cardTotalroot}>
+                    <Card>
+                        <CardContent className={classes.cardTotalcontent}>
+                            <Typography variant="subtitle1" gutterBottom>
+                                Food Keyword : {foodKeyword}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={6} className={classes.cardTotalroot}>
+                    <Card>
+                        <CardContent className={classes.cardTotalcontent}>
+                            <Typography variant="subtitle1" gutterBottom>
+                                Maximum Keyword Matching Score : {maxScore}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Fragment>
+        );
+    }
+
+    resultCard(document) {
+        const { classes } = this.props;
+
+        const source = document['_source'],
+            score = document['_score'],
+            id = source['reviewid'];
+
+        var text = document['highlight']["reviewtext"];
+            // text = text.replace("<keyword>", "<b>");
+            // text = text.replace("</keyword>", "</b>");
+
+        var modifiedDate = new Date(source['modified'] / 1000000);
+            modifiedDate = modifiedDate.toUTCString();
+
+        return (
+            <Grid item xs={12} key={id}>
+                <Card className={classes.cardroot}>
+                    <CardHeader
+                        title={"Review ID : " + id}
+                        subheader={"Keyword Matching Score : " + score}
+                    />
+                    <CardContent className={classes.cardcontent}>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Review Text
+                        </Typography>
+                        <Typography variant="body1" component="p" className={classes.reviewtext}>
+                            {text}
+                        </Typography>
+                        <Typography variant="body2" component="p" color="secondary" className={classes.modifieddate}>
+                            Last modified : {modifiedDate}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        );
+    }
+
+    handlePagination() {
+        const { classes } = this.props;
+        const { page } = this.state;
+
+        return (
+            <Grid item xs={12}>
+                <Pagination count={10} size="large" className={classes.paginationroot} page={page} onChange={this.handleChangePage} />
+            </Grid>
+        )
+    }
+
+    handleHasresult() {
+        const { resultDatas, page } = this.state;
+        return (
+            <Fragment>
+                {this.totalCard()}
+                {this.handlePagination()}
+                {resultDatas.slice(10 * (page - 1), 10 * page).map(document => (
+                    this.resultCard(document)
+                ))}
+                {this.handlePagination()}
+            </Fragment>
+        )
+    }
+
+    handleNoresult() {
+        const { classes } = this.props;
+        const { errorMessage } = this.state;
+
+        return (
+            <Grid item xs={12}>
+                <Card className={classes.cardroot}>
+                    <CardHeader
+                        title={"Error Message : " + errorMessage }
+                    />
+                </Card>
+            </Grid>
+        )
+    }
+
+    handleEmpty() {
+        return (
+            <Fragment></Fragment>
+        )
+    }
+
     render() {
         const { classes } = this.props;
-        const { foodKeyword, reviewText } = this.state;
-
+        const { resultDatas, errorMessage, page } = this.state;
+        console.log(page)
         return (
             <div className={classes.root}>
                 <Container>
                     <Grid container spacing={3}>
                         <Grid item xs={12}>
-                            <h1>Search Food Review by Keyword</h1>
+                            <h1>Search Top 100 Food Reviews by Matching a Keyword</h1>
                         </Grid>
-                        <Grid item xs={12}>
-                            <Paper component="form" className={classes.searchroot}>
-                                <InputBase
-                                    className={classes.input}
-                                    placeholder="Search Food Reviews Ex. 1"
-                                    autoComplete="1"
-                                />
-                                <Divider className={classes.divider} orientation="vertical" />
-                                <IconButton color="primary" type="submit" className={classes.iconButton} aria-label="directions">
-                                    <SearchIcon />
-                                </IconButton>
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Card className={classes.cardroot}>
-                                <CardContent>
-                                    <Typography variant="h5" gutterBottom>
-                                        Food Keyword
-                                </Typography>
-                                    <Typography variant="h6" className={classes.reviewtext}>
-                                        {foodKeyword}
-                                    </Typography>
-                                    <Typography variant="h5" gutterBottom>
-                                        Review Text
-                                </Typography>
-                                    <Typography variant="h6" component="p" className={classes.reviewtext}>
-                                        {reviewText}
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
+                        {this.searchBox()}
+                        {(resultDatas && !errorMessage) ? this.handleHasresult() : this.handleEmpty()}
+                        {(!resultDatas && errorMessage)  ? this.handleNoresult() : this.handleEmpty()}
                     </Grid>
                 </Container>
             </div>
